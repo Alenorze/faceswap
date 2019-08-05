3b42ad7d9cad538947071c4ce115a8de42530df9
Alenorze <alenorze@gmail.com>
2019-08-05 11:17:46 +0300

    fix

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,8 +1,9 @@
 import os
 import subprocess
+import uuid
 
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
@@ -21,5 +22,10 @@ class HomeView(TemplateView):
     
         default_storage.save('img/file_1.jpg', ContentFile(file_1.read()))
         default_storage.save('img/file_2.jpg', ContentFile(file_2.read()))
-        subprocess.run('python main.py --src ../media/img/file_1.jpg --dst ../media/img/file_2.jpg --out ../media/result/result.jpg --correct_color', shell=True, check=True)
-        return HttpResponseRedirect('/')
+        result = str(uuid.uuid4())
+        com = 'python main.py --src ../media/img/file_1.jpg --dst ../media/img/file_2.jpg --out ../media/result/' + result + '.jpg --correct_color'
+        subprocess.run(com, shell=True, check=True)
+        context = {
+            'img_name': result
+        }
+        return render(request, 'base.html', context=context)
