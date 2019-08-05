1d3161c6e2e03172366747c75c69d09c1643ab14
Alenorze <alenorze@gmail.com>
2019-08-05 11:01:34 +0300

    fix

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -13,7 +13,7 @@ SECRET_KEY = '4yc&#n6%6-2gx4#zp1lry7@!vhblo8nb^gh*-8ny0$bza&)qbs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
